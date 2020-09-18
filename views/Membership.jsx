f43e5c1c83367c6570fe01d4725eacac305afea0
Betty Beshah <bettybeshah@Bettys-MacBook-Pro.local>
2020-09-18 12:03:10 -0400

    updates

--- views/Membership.jsx
+++ views/Membership.jsx
@@ -1,11 +1,21 @@
 const React = require('react')
+const Layout = require('./Layout.jsx')
 
 class Membership extends React.Component {
     render(){
         return(
+            <Layout>
+            <html>
+                <head>
+                <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=yes"/>
+                </head>
             <div>
                 <h1>Hello</h1>
+                <a>see here</a>
+                
             </div>
+            </html>
+            </Layout>
         )
     }
 }
