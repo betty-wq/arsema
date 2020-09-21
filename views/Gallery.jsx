85f5d1eda31ee7085c8499fa3906916642070b62
Betty Beshah <bettybeshah@Bettys-MacBook-Pro.local>
2020-09-21 17:47:55 -0400

    updates

--- views/Gallery.jsx
+++ views/Gallery.jsx
@@ -38,11 +38,71 @@ class Gallery extends React.Component {
                 </nav>
 
                 <div class="images">
-                    <div class="image">
+                    {/* <div class="image">
                     <img src='https://i.imgur.com/AzhY565.png' width="100%" height="100%"/>
                     </div>
                     <div class="image">
                     <img src='https://i.imgur.com/c77xpmQ.png' width="100%"/>
+                    </div> */}
+                    <div class="image">
+                    <img src='https://i.imgur.com/AzhY565.png' width="100%" height="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/jUREyjq.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/ZPoBT6m.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/3X9jscb.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/FW9Jiri.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/Fi0ZJZE.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/tHXGWZA.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/daOZHQh.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/O7CMB3B.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/xLv2CxU.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/ABXOPPn.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/fOzs6kQ.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/PUbknnV.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/uPTdWIZ.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/CR8MBWo.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/9CWEK5D.jpg' width="100%"/>
+                    </div>
+                    <div class="image">
+                        <img src="https://i.imgur.com/IPKtQpY.jpg" width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/c77xpmQ.png' width="100%"/>
+                    </div>
+                    <div class="image">
+                    <img src='https://i.imgur.com/yaGsTjD.jpg' width="100%"/>
+                    </div>
+                    <div>
+                        <img src="https://i.imgur.com/CT9WwPu.jpg" width="100%"/>
                     </div>
                     <button class="ams ag"><a href="https://www.facebook.com/pg/Kidist-Arsema-wa-Kidus-Kirkos-EOTC-361541617371571/photos/?ref=page_internal">ተጨማሪ ምስሎችን ለመመልከት ይህንን ይጫኑ</a></button> 
                </div>
