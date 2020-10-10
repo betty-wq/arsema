02bee0738fa22a0685e1d4840c2c14c915c6f87f
Betty Beshah <bettybeshah@Bettys-MacBook-Pro.local>
2020-10-10 16:59:25 -0400

    update

--- views/Gallery.jsx
+++ views/Gallery.jsx
@@ -44,12 +44,12 @@ class Gallery extends React.Component {
                 <div class="gallery_main">
                 <div class="gallery_card">
                     <a href="/logs/Photo">
-                    <img src="https://thumbs.dreamstime.com/b/black-icon-camera-cartoon-vintage-vector-graphic-sdesign-93594588.jpg"></img>
+                    <img src="https://i.imgur.com/VmgEFnt.jpg"></img>
                     </a>
                 </div>
                 <div class="gallery_card">
                     <a href="/logs/Videos">
-                    <img src="https://www.netclipart.com/pp/m/176-1769148_btsmoviemaking-com-clip-art-old-film-camera.png"></img>
+                    <img src="https://i.imgur.com/08XRpJl.png"></img>
                     </a>
                 </div>
                 </div>
