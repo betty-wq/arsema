15869c3c8d7a0861f8ecc16e702067b626ec50ca
Betty Beshah <bettybeshah@Bettys-MacBook-Pro.local>
2020-10-15 19:40:34 -0400

    update

--- views/Index.jsx
+++ views/Index.jsx
@@ -40,7 +40,7 @@ class Index extends React.Component {
                 </ul>
                 </nav>
                 <div class="welcometitle">
-                    <img class="cross" src="https://www.hamillgallery.com/ETHIOPIAN/EthiopianLargeProcCrosses/EthiopianCross324wt.jpg"></img>
+                    <img class="cross" src="https://i.imgur.com/qzcLHTn.png"></img>
                 <h1 class=" animated bounce infinite">እንኳን ደህና መጡ</h1>
                 <h1 class="welcomeh">Welcome!</h1>
                 <p>We are an Ethiopian Orthodox Church located in Alexandria Virginia. please visit our about us page to learn more.</p>
